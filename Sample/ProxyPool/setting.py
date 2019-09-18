84697434ce33190bf8f12b4d1e7a45c655e7e1d3
Administrator <5706153@qq.com>
2019-09-18 22:13:28 +0800

    201909181651增加

---
+++ Sample/ProxyPool/setting.py
@@ -0,0 +1,15 @@
+# Redis数据库地址
+REDIS_HOST = '127.0.0.1'
+
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，如无填None
+REDIS_PASSWORD = 'wei8899'
+
+REDIS_KEY = 'proxies'
+
+# 代理分数
+MAX_SCORE = 100
+MIN_SCORE = 0
+INITIAL_SCORE = 10
